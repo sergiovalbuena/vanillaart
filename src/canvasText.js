09aaa65d479ce6e68af4e88ac1c776c27c43dcfd
sergiovalbuena <iosergio.valbuena@gmail.com>
2024-03-07 19:29:09 -0800

    Add dynamic text rendering to canvas

--- src/canvasText.js
+++ src/canvasText.js
@@ -5,14 +5,40 @@ const settings = {
   animate: true,
 };
 
+let text = "S";
+let fontSize = 1200;
+let fontFamily = "Arial";
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = "black";
     context.fillRect(0, 0, width, height);
 
-    context.font = "100px Arial";
     context.fillStyle = "white";
-    context.fillText("Hello Canvas", 100, 100);
+    context.font = `${fontSize}px ${fontFamily}`;
+    context.textBaseline = "top";
+    //context.textAlign = "center";
+
+    const metrics = context.measureText(text);
+    //console.log(metrics);
+    const mx = metrics.actualBoundingBoxLeft * -1;
+    const my = metrics.actualBoundingBoxAscent * -1;
+    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const mh =
+      metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    const x = (width - mw) * 0.5 - mx;
+    const y = (height - mh) * 0.5 - my;
+
+    context.save();
+    context.translate(x, y);
+
+    context.beginPath();
+    context.rect(mx, my, mw, mh);
+    context.stroke();
+
+    context.fillText(text, 0, 0);
+    context.restore();
   };
 };
 sketch();
